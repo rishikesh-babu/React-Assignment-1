82c23ed971e1998bbe44314883dce1ec404f8aaf
rishikesh-babu <rishikeshbabu2004@gmail.com>
2024-08-04 20:55:49 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Loginbox from "./Login/Loginbox";
+import Signup from "./Signup/Signup";
+import Footer from "./Footer/Footer";
+import Reactnavbar from "./Navbar/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer2 from "./Footer/Footer2";
+import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="nav-container">
+        <Reactnavbar />
+      </div>
+      <div className="main">
+        <Loginbox />
+        <Signup />
+      </div>
+      <div>
+        <Footer />
+        {/* <Footer2 /> */}
+      </div>
+    </>
   );
 }
 
